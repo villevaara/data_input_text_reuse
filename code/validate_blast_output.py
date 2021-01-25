c3a4005bb1ced030a1ddcc130d2a2a6262672fcd
Ville Vaara <villepvaara@gmail.com>
2021-01-25 15:50:12 +0200

    Update validation script outputs.

--- code/validate_blast_output.py
+++ code/validate_blast_output.py
@@ -3,6 +3,7 @@ from lib.blast_datareader import (
     validate_blast_iter_data,
     get_tar_datafiles)
 import csv
+from datetime import date
 
 
 parser = argparse.ArgumentParser(
@@ -23,10 +24,15 @@ for tarfile in tarfiles_in_path:
     if validates['validates'] is not True:
         bad_results.append({
             'iter': this_iter, 'n_bad': validates['n_bad']})
+        print("\n!! iter: " + str(this_iter) +
+              "n_bad:" + str(validates['n_bad']) + "\n")
 
 br_sorted = sorted(bad_results, key=lambda k: k['iter'])
 
-with open((args.logdir + '/iters_invalid.csv'), 'w', newline='') as csvfile:
+datestring = date.today().strftime("%y%d%m")
+log_fname = args.logdir + '/iters_invalid_' + datestring + '.csv'
+
+with open(log_fname, 'w', newline='') as csvfile:
     fieldnames = ['iter', 'n_bad']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
