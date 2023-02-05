48065706fe3f4ed3d00ea609f289e924a236e48e
Ville Vaara <villepvaara@gmail.com>
2023-02-05 20:50:55 +0200

    Change data picker to input of file with ids.

--- code/get_tr_data_output.py
+++ code/get_tr_data_output.py
@@ -67,19 +67,47 @@ def get_single_input_output(input_filename, id_to_get):
     return outputdata
 
 
+def get_single_input_output_manyids(input_filename, ids_to_get):
+    outputdata = []
+    tar_cont = get_single_tar_json_contents(filename)[0]
+    for item in tar_cont:
+        data_found = False
+        if get_actual_eebo_id(item['text1_id']) in ids_to_get:
+            item_position = "1"
+            data_found = True
+        elif get_actual_eebo_id(item['text2_id']) in ids_to_get:
+            item_position = "2"
+            data_found = True
+        if data_found:
+            fname = input_filename.split("/")[-1]
+            outputrow = get_output_row(item, item_position, fname)
+            outputdata.append(outputrow)
+    return outputdata
+
+
+def read_idfile(idfile):
+    with open(idfile, 'r') as f:
+        ids = f.readlines()
+        ids_filtered = [id.strip() for id in ids if id != ""]
+    return ids_filtered
+
+
 parser = argparse.ArgumentParser(
     description="Fetch single ECCO id text reuse data.")
 parser.add_argument("--indexfile", help="Index file location",
                     default="../data/work/text_index.json")
-parser.add_argument("--id", help="ECCO / EEBO id of interest", required=True)
+# parser.add_argument("--id", help="ECCO / EEBO id of interest", required=False)
+parser.add_argument("--idfile", help="ECCO / EEBO ids of interest listed in a file", required=True)
 parser.add_argument("--outputpath", help="Output files location",
                     default="../data/work/tr_output")
 args = parser.parse_args()
 
 
 index_datafile = args.indexfile
-id_to_get = args.id
+# id_to_get = args.id
 outputpath = args.outputpath
+ids_to_get = read_idfile(args.idfile)
+
 
 create_dir_if_not_exists(outputpath)
 
@@ -87,17 +115,19 @@ with open(index_datafile, 'r') as jsondata:
     data_index = json.load(jsondata)
 
 
-if id_to_get not in data_index.keys():
-    sys.exit("ID not found in text reuse data.")
+# if id_to_get not in data_index.keys():
+#    sys.exit("ID not found in text reuse data.")
 
-files_of_interest = data_index[id_to_get]
+files_of_interest = list()
+for id_to_get in ids_to_get:
+    files_of_interest.append(data_index[id_to_get])
 
 i = 0
 max_i = len(files_of_interest)
 for filename in files_of_interest:
     i += 1
     print("Reading " + str(i) + "/" + str(max_i) + " : " + filename)
-    outputdata = get_single_input_output(filename, id_to_get)
+    outputdata = get_single_input_output_manyids(filename, ids_to_get)
     if len(outputdata) > 0:
         outputfname = (
             outputpath + "/results_" + filename.split("/")[-1].split(".")[0] +
